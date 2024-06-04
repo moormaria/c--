bf8249355744b745207c7d109121ba9ba0f85f34
moormaria <mashunya.moor@gmail.com>
2024-06-04 22:50:49 +0300

    arkanoid

---
+++ arkanoid/source/main.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <time.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include "Rectangle.h"
+#include "Player.h"
+#include "Ball.h"
+#include "Block.h"
+
+using namespace std;
+
+int main() {
+	return 0;
+}
